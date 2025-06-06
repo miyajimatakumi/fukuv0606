88a8dd470b65eb1b29b98e6622705af93408bf89
miyajimatakumi <m.miyajima.takumi@gmail.com>
2025-06-06 13:33:18 +0900

    v1

--- fukuv0606/Form1.cs
+++ fukuv0606/Form1.cs
@@ -2,9 +2,29 @@ namespace fukuv0606
 {
     public partial class Form1 : Form
     {
+        int vx = -4;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            else if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "miyajimatakumi";
         }
     }
 }
